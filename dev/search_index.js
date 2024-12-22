b8390429da816b750854f7706f02e2efb2e99ea4
Documenter.jl <documenter@juliadocs.github.io>
2024-12-22 14:31:07 +0000

    build based on 94a49c6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BooleanInference","category":"page"},{"location":"#BooleanInference","page":"Home","title":"BooleanInference","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BooleanInference.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BooleanInference]","category":"page"}]
+}
