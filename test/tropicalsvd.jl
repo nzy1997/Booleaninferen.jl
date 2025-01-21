528191da71a388afc7b107a53afbafa23266b90d
GiggleLiu <cacate0129@gmail.com>
2025-01-21 23:42:19 +0800

    speed up

--- test/tropicalsvd.jl
+++ test/tropicalsvd.jl
@@ -4,8 +4,6 @@ using Random
 using BooleanInference
 using BooleanInference.OptimalBranchingCore
 
-
-
 @testset "tropical_svd" begin
     m = 5
     n = 5
@@ -17,9 +15,9 @@ using BooleanInference.OptimalBranchingCore
 end
 
 @testset "tropical_svd" begin
-    m = 20
-    k= 4
-    n = 10
+    m = 50
+    k= 6
+    n = 50
     Random.seed!(1234)
     a = [TropicalAndOr(rand()>0.5) for i in 1:m, j in 1:k]
     b = [TropicalAndOr(rand()>0.5) for i in 1:k, j in 1:n]
